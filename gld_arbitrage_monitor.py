992161956142709b08fa83012eecaf359cb5fb26
VincentHuang1 <vincent.propertysales@gmail.com>
2025-05-27 15:31:20 +1000

    Update gld_arbitrage_monitor.py

--- gld_arbitrage_monitor.py
+++ gld_arbitrage_monitor.py
@@ -79,5 +79,9 @@ def index():
         row_class=row_class
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
